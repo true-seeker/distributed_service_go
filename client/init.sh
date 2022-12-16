377e264b5f42dd61f8b9381c00a6a163733d8b05
Kirill Murzin <murz.kirill@yandex.ru>
2022-12-16 16:59:56 +0500

    client docker

---
+++ client/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd gRPC
+protoc --go_out=../backpackTaskGRPC/ --go_opt=paths=source_relative --go-grpc_out=../backpackTaskGRPC/ --go-grpc_opt=paths=source_relative grpc.proto
+cd ..
+go build
+./client
