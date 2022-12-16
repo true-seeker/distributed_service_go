735b8378a068f03b60e965291f87ddcadcaf326c
Kirill Murzin <murz.kirill@yandex.ru>
2022-12-16 14:42:33 +0500

    external task generation

--- server/services/WebServerService.go
+++ server/services/WebServerService.go
@@ -11,8 +11,13 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "ok")
 }
 
+func generateTaskView(w http.ResponseWriter, r *http.Request) {
+	GenerateTask(5000)
+}
+
 func StartWebServerListener() {
 	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/generateTask", generateTaskView)
 	fmt.Println("http listener started")
 	err := http.ListenAndServe(":3333", nil)
 	FailOnError(err, "Cant start http listener")
