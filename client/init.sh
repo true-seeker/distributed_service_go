c930e06468ed4e2eb20e8e42f57349f28e4c7a83
Kirill Murzin <murz.kirill@yandex.ru>
2022-12-16 17:04:08 +0500

    Revert "client docker"
    This reverts commit 377e264b5f42dd61f8b9381c00a6a163733d8b05.

--- client/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd gRPC
-protoc --go_out=../backpackTaskGRPC/ --go_opt=paths=source_relative --go-grpc_out=../backpackTaskGRPC/ --go-grpc_opt=paths=source_relative grpc.proto
-cd ..
-go build
-./client
